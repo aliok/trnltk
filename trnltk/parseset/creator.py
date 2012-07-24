374fa6b3a05289d2d4c859bc446e204adf68a495
Ali Ok <aliok@apache.org>
2012-07-24 18:36:50 +0200

    Skip free transition suffixes in parsesets

--- trnltk/parseset/creator.py
+++ trnltk/parseset/creator.py
@@ -1,4 +1,5 @@
 from trnltk.parseset.xmlbindings import *
+from trnltk.suffixgraph.suffixgraphmodel import FreeTransitionSuffix
 
 class ParseSetCreator(object):
     def create_word_binding_from_token(self, word_str, token):
@@ -16,10 +17,14 @@ class ParseSetCreator(object):
 
         if token.get_transitions():
             for transition in token.get_transitions():
+                if isinstance(transition.suffix_form_application.suffix_form.suffix, FreeTransitionSuffix):
+                    continue
+
                 suffix_name = transition.suffix_form_application.suffix_form.suffix.name
+                suffix_pretty_name = transition.suffix_form_application.suffix_form.suffix.pretty_name
                 suffix_form = transition.suffix_form_application.suffix_form.form
                 suffix_application = transition.suffix_form_application.applied_suffix_form
-                suffix = SuffixBinding(suffix_name, suffix_form, suffix_application)
+                suffix = SuffixBinding(suffix_name, suffix_pretty_name, suffix_form, suffix_application)
                 word.suffixes.append(suffix)
         return word
 
