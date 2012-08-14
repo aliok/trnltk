b95da5ee4b6a87c906f10f11223ea039bfe5e063
Ali Ok <aliok@apache.org>
2012-08-15 00:44:55 +0200

    Context reverse stats

--- trnltk/statistics/contextstats.py
+++ trnltk/statistics/contextstats.py
@@ -4,7 +4,6 @@ import pymongo
 class WordBigramQueryBuilder(object):
     def __init__(self):
         self._query = {}
-        pass
 
     def surface(self, str_value, syntactic_category=None):
         self._query['item_1.word.surface.value'] = str_value
@@ -70,7 +69,6 @@ class BigramContextProbabilityGenerator(object):
         #don't attempt parsing the leading_context yet
         leading_word = leading_context[0][0]
         following_word = following_context[0][0]
-
 
         surface_str = morpheme_container.get_surface_so_far()
         surface_syn_cat = morpheme_container.get_surface_syntactic_category()
@@ -165,7 +163,7 @@ class ContextParsingLikelihoodCalculator(object):
         self.collection.ensure_index([
             ("item_0.word.surface.value", pymongo.ASCENDING),
             ("item_0.word.surface.syntactic_category", pymongo.ASCENDING)
-        ], name="surface_surface_index", drop_dups=True)
+        ], name="word_surface_index", drop_dups=True)
 
         query_given_leading_surface = WordBigramQueryBuilder().given_surface(leading_surface_str, leading_surface_syn_cat).build()
         count_given_leading_surface = float(self.collection.find(query_given_leading_surface).count())
@@ -179,7 +177,8 @@ class ContextParsingLikelihoodCalculator(object):
             ("item_1.word.surface.syntactic_category", pymongo.ASCENDING),
         ], name="surface_surface_index", drop_dups=True)
 
-        query_surface_given_leading_surface = WordBigramQueryBuilder().surface(surface_str, surface_syntactic_category).given_surface(leading_surface_str, leading_surface_syn_cat).build()
+        query_surface_given_leading_surface = WordBigramQueryBuilder().surface(surface_str, surface_syntactic_category).given_surface(leading_surface_str,
+            leading_surface_syn_cat).build()
         count_surface_given_leading_surface = float(self.collection.find(query_surface_given_leading_surface).count())
         return count_surface_given_leading_surface
 
@@ -191,7 +190,8 @@ class ContextParsingLikelihoodCalculator(object):
             ("item_1.word.stem.syntactic_category", pymongo.ASCENDING),
         ], name="surface_stem_index", drop_dups=True)
 
-        query_stem_given_leading_surface = WordBigramQueryBuilder().stem(stem_str, stem_syntactic_category).given_surface(leading_surface_str, leading_surface_syn_cat).build()
+        query_stem_given_leading_surface = WordBigramQueryBuilder().stem(stem_str, stem_syntactic_category).given_surface(leading_surface_str,
+            leading_surface_syn_cat).build()
         count_stem_given_leading_surface = float(self.collection.find(query_stem_given_leading_surface).count())
         return count_stem_given_leading_surface
 
@@ -203,7 +203,8 @@ class ContextParsingLikelihoodCalculator(object):
             ("item_1.word.lemma_root.syntactic_category", pymongo.ASCENDING),
         ], name="surface_lemma_root_index", drop_dups=True)
 
-        query_lexeme_given_leading_surface = WordBigramQueryBuilder().lemma_root(lemma_root_str, lemma_root_syntactic_category).given_surface(leading_surface_str, leading_surface_syn_cat).build()
+        query_lexeme_given_leading_surface = WordBigramQueryBuilder().lemma_root(lemma_root_str, lemma_root_syntactic_category).given_surface(
+            leading_surface_str, leading_surface_syn_cat).build()
         count_lexeme_given_leading_surface = float(self.collection.find(query_lexeme_given_leading_surface).count())
         return count_lexeme_given_leading_surface
 
@@ -226,7 +227,8 @@ class ContextParsingLikelihoodCalculator(object):
             ("item_1.word.surface.syntactic_category", pymongo.ASCENDING),
         ], name="stem_surface_index", drop_dups=True)
 
-        query_surface_given_leading_stem = WordBigramQueryBuilder().surface(surface_str, surface_syntactic_category).given_stem(leading_stem_str, leading_stem_syn_cat).build()
+        query_surface_given_leading_stem = WordBigramQueryBuilder().surface(surface_str, surface_syntactic_category).given_stem(leading_stem_str,
+            leading_stem_syn_cat).build()
         count_surface_given_leading_stem = float(self.collection.find(query_surface_given_leading_stem).count())
         return count_surface_given_leading_stem
 
@@ -238,7 +240,8 @@ class ContextParsingLikelihoodCalculator(object):
             ("item_1.word.stem.syntactic_category", pymongo.ASCENDING),
         ], name="stem_stem_index", drop_dups=True)
 
-        query_stem_given_leading_stem = WordBigramQueryBuilder().stem(stem_str, stem_syntactic_category).given_stem(leading_stem_str, leading_stem_syn_cat).build()
+        query_stem_given_leading_stem = WordBigramQueryBuilder().stem(stem_str, stem_syntactic_category).given_stem(leading_stem_str,
+            leading_stem_syn_cat).build()
         count_stem_given_leading_stem = float(self.collection.find(query_stem_given_leading_stem).count())
         return count_stem_given_leading_stem
 
@@ -250,7 +253,8 @@ class ContextParsingLikelihoodCalculator(object):
             ("item_1.word.lemma_root.syntactic_category", pymongo.ASCENDING),
         ], name="stem_lemma_root_index", drop_dups=True)
 
-        query_lexeme_given_leading_stem = WordBigramQueryBuilder().lemma_root(lemma_root_str, lemma_root_syntactic_category).given_stem(leading_stem_str, leading_stem_syn_cat).build()
+        query_lexeme_given_leading_stem = WordBigramQueryBuilder().lemma_root(lemma_root_str, lemma_root_syntactic_category).given_stem(leading_stem_str,
+            leading_stem_syn_cat).build()
         count_lexeme_given_leading_stem = float(self.collection.find(query_lexeme_given_leading_stem).count())
         return count_lexeme_given_leading_stem
 
@@ -273,7 +277,8 @@ class ContextParsingLikelihoodCalculator(object):
             ("item_1.word.surface.syntactic_category", pymongo.ASCENDING),
         ], name="lemma_root_surface_index", drop_dups=True)
 
-        query_surface_given_leading_lexeme = WordBigramQueryBuilder().surface(surface_str, surface_syntactic_category).given_lemma_root(leading_lemma_root_str, leading_lemma_root_syn_cat).build()
+        query_surface_given_leading_lexeme = WordBigramQueryBuilder().surface(surface_str, surface_syntactic_category).given_lemma_root(leading_lemma_root_str,
+            leading_lemma_root_syn_cat).build()
         count_surface_given_leading_lexeme = float(self.collection.find(query_surface_given_leading_lexeme).count())
         return count_surface_given_leading_lexeme
 
@@ -285,7 +290,8 @@ class ContextParsingLikelihoodCalculator(object):
             ("item_1.word.stem.syntactic_category", pymongo.ASCENDING),
         ], name="lemma_root_stem_index", drop_dups=True)
 
-        query_stem_given_leading_lexeme = WordBigramQueryBuilder().stem(stem_str, stem_syntactic_category).given_lemma_root(leading_lemma_root_str, leading_lemma_root_syn_cat).build()
+        query_stem_given_leading_lexeme = WordBigramQueryBuilder().stem(stem_str, stem_syntactic_category).given_lemma_root(leading_lemma_root_str,
+            leading_lemma_root_syn_cat).build()
         count_stem_given_leading_lexeme = float(self.collection.find(query_stem_given_leading_lexeme).count())
         return count_stem_given_leading_lexeme
 
@@ -297,20 +303,22 @@ class ContextParsingLikelihoodCalculator(object):
             ("item_1.word.lemma_root.syntactic_category", pymongo.ASCENDING),
         ], name="lemma_root_lemma_root_index", drop_dups=True)
 
-        query_lexeme_given_leading_lexeme = WordBigramQueryBuilder().lemma_root(lemma_root_str, lemma_root_syntactic_category).given_lemma_root(leading_lemma_root_str, leading_lemma_root_syn_cat).build()
+        query_lexeme_given_leading_lexeme = WordBigramQueryBuilder().lemma_root(lemma_root_str, lemma_root_syntactic_category).given_lemma_root(
+            leading_lemma_root_str, leading_lemma_root_syn_cat).build()
         count_lexeme_given_leading_lexeme = float(self.collection.find(query_lexeme_given_leading_lexeme).count())
         return count_lexeme_given_leading_lexeme
 
 
     def _p_word_given_leading_surface(self, morpheme_container, surface_str, surface_syn_cat, stem_str, stem_syn_cat, lemma_root_str, lemma_root_syn_cat):
-
         leading_surface_str = morpheme_container.get_surface_so_far()
         leading_surface_syn_cat = morpheme_container.get_surface_syntactic_category()
 
         count_given_leading_surface = self._count_given_leading_surface(leading_surface_str, leading_surface_syn_cat)
-        count_surface_given_leading_surface = self._count_surface_given_leading_surface(leading_surface_str, leading_surface_syn_cat, surface_str, surface_syn_cat)
+        count_surface_given_leading_surface = self._count_surface_given_leading_surface(leading_surface_str, leading_surface_syn_cat, surface_str,
+            surface_syn_cat)
         count_stem_given_leading_surface = self._count_stem_given_leading_surface(leading_surface_str, leading_surface_syn_cat, stem_str, stem_syn_cat)
-        count_lexeme_given_leading_surface = self._count_lexeme_given_leading_surface(leading_surface_str, leading_surface_syn_cat, lemma_root_str, lemma_root_syn_cat)
+        count_lexeme_given_leading_surface = self._count_lexeme_given_leading_surface(leading_surface_str, leading_surface_syn_cat, lemma_root_str,
+            lemma_root_syn_cat)
 
         # P(surface + surface_syn_cat | leading surface + syn_cat)
         p_surface_given_leading_surface = count_surface_given_leading_surface / count_given_leading_surface if count_given_leading_surface else 0.0
@@ -326,7 +334,6 @@ class ContextParsingLikelihoodCalculator(object):
         return p_word_with_leading_surface
 
     def _p_word_given_leading_stem(self, morpheme_container, surface_str, surface_syn_cat, stem_str, stem_syn_cat, lemma_root_str, lemma_root_syn_cat):
-
         leading_stem_str = morpheme_container.get_stem()
         leading_stem_syn_cat = morpheme_container.get_stem_syntactic_category()
 
@@ -349,14 +356,15 @@ class ContextParsingLikelihoodCalculator(object):
         return p_word_with_leading_stem
 
     def _p_word_given_leading_lexeme(self, morpheme_container, surface_str, surface_syn_cat, stem_str, stem_syn_cat, lemma_root_str, lemma_root_syn_cat):
-
         leading_lemma_root_str = morpheme_container.get_root().lexeme.root
         leading_lemma_root_syn_cat = morpheme_container.get_root().lexeme.syntactic_category
 
         count_given_leading_lexeme = self._count_given_leading_lexeme(leading_lemma_root_str, leading_lemma_root_syn_cat)
-        count_surface_given_leading_lexeme = self._count_surface_given_leading_lexeme(leading_lemma_root_str, leading_lemma_root_syn_cat, surface_str, surface_syn_cat)
+        count_surface_given_leading_lexeme = self._count_surface_given_leading_lexeme(leading_lemma_root_str, leading_lemma_root_syn_cat, surface_str,
+            surface_syn_cat)
         count_stem_given_leading_lexeme = self._count_stem_given_leading_lexeme(leading_lemma_root_str, leading_lemma_root_syn_cat, stem_str, stem_syn_cat)
-        count_lexeme_given_leading_lexeme = self._count_lexeme_given_leading_lexeme(leading_lemma_root_str, leading_lemma_root_syn_cat, lemma_root_str, lemma_root_syn_cat)
+        count_lexeme_given_leading_lexeme = self._count_lexeme_given_leading_lexeme(leading_lemma_root_str, leading_lemma_root_syn_cat, lemma_root_str,
+            lemma_root_syn_cat)
 
         # P(surface + surface_syn_cat | leading lemma + syn_cat)
         p_surface_given_leading_lexeme = count_surface_given_leading_lexeme / count_given_leading_lexeme if count_given_leading_lexeme else 0.0
@@ -375,11 +383,13 @@ class ContextParsingLikelihoodCalculator(object):
         total = 0.0
 
         for morpheme_container in parse_results_of_leading_word:
-            p_word_given_leading_surface = self._p_word_given_leading_surface(morpheme_container, surface_str, surface_syn_cat, stem_str, stem_syn_cat, lemma_root_str, lemma_root_syn_cat)
-            p_word_given_leading_stem = self._p_word_given_leading_stem(morpheme_container, surface_str, surface_syn_cat, stem_str, stem_syn_cat, lemma_root_str, lemma_root_syn_cat)
-            p_word_given_leading_lexeme = self._p_word_given_leading_lexeme(morpheme_container, surface_str, surface_syn_cat, stem_str, stem_syn_cat, lemma_root_str, lemma_root_syn_cat)
+            p_word_given_leading_surface = self._p_word_given_leading_surface(morpheme_container, surface_str, surface_syn_cat, stem_str, stem_syn_cat,
+                lemma_root_str, lemma_root_syn_cat)
+            p_word_given_leading_stem = self._p_word_given_leading_stem(morpheme_container, surface_str, surface_syn_cat, stem_str, stem_syn_cat, lemma_root_str
+                , lemma_root_syn_cat)
+            p_word_given_leading_lexeme = self._p_word_given_leading_lexeme(morpheme_container, surface_str, surface_syn_cat, stem_str, stem_syn_cat,
+                lemma_root_str, lemma_root_syn_cat)
 
             total += 0.55 * p_word_given_leading_surface + 0.3 * p_word_given_leading_stem + 0.15 * p_word_given_leading_lexeme
-
 
         return total / float(len(parse_results_of_leading_word))
