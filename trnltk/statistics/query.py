e775f4e167ca7c2435b596ec61675cb485afe884
Ali Ok <aliok@apache.org>
2012-08-16 00:45:40 +0200

    Ngram queries

---
+++ trnltk/statistics/query.py
@@ -0,0 +1,136 @@
+# coding=utf-8
+import pymongo
+
+class WordNGramQueryContainerItem(object):
+    def __init__(self, str_type, include_syntactic_category):
+        self.str_type = str_type
+        self.include_syntactic_category = include_syntactic_category
+
+    def __str__(self):
+        if self.include_syntactic_category:
+            return "({}, {})".format(self.str_type, "syntactic_category")
+        else:
+            return "({})".format(self.str_type)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class WordNGramQueryContainer(object):
+    def __init__(self, n):
+        assert n>0
+        self._n = n
+        self._target_item = None
+        self._given_items = []
+
+    def _add_given(self, type, include_syntactic_category=False):
+        self._given_items.append(WordNGramQueryContainerItem(type, include_syntactic_category))
+        return self
+
+    def _add_target(self, type, include_syntactic_category=False):
+        if self._target_item:
+            raise Exception("Target item is already set!")
+        self._target_item = WordNGramQueryContainerItem(type, include_syntactic_category)
+        return self
+
+    def given_surface(self, include_syntactic_category=False):
+        return self._add_given('surface', include_syntactic_category)
+
+    def given_stem(self, include_syntactic_category=False):
+        return self._add_given('stem', include_syntactic_category)
+
+    def given_lemma_root(self, include_syntactic_category=False):
+        return self._add_given('lemma_root', include_syntactic_category)
+
+    def target_surface(self, include_syntactic_category=False):
+        return self._add_target('surface', include_syntactic_category)
+
+    def target_stem(self, include_syntactic_category=False):
+        return self._add_target('stem', include_syntactic_category)
+
+    def target_lemma_root(self, include_syntactic_category=False):
+        return self._add_target('lemma_root', include_syntactic_category)
+
+    def create_context(self, target_comes_after):
+        assert self._n == (1 if self._target_item else 0) + len(self._given_items)
+
+        index_name = "word{}GramIdx".format(self._n)
+        keys = []
+
+        target_item_index = self._n - 1 if target_comes_after else 0
+        item_index_range = range(0, target_item_index) if target_comes_after else range(1, self._n)
+
+        if not self._target_item:
+            target_item_index = -1
+            item_index_range = range(0, self._n)
+
+        if self._target_item:
+            target_item_index_part, target_item_keys = self._build_key(self._target_item, target_item_index)
+            keys.extend(target_item_keys)
+            index_name += target_item_index_part
+
+        for index, item in enumerate(self._given_items):
+            item_index = item_index_range[index]
+            item = self._given_items[index]
+            item_index_part, item_keys = self._build_key(item, item_index)
+            keys.extend(item_keys)
+            index_name += item_index_part
+
+        return index_name, keys
+
+    def _build_key(self, query_item, index):
+        index_part = "_{}_{}".format(index, query_item.str_type)
+        keys = ["item_{}.word.{}.value".format(index, query_item.str_type)]
+        if query_item.include_syntactic_category:
+            keys.append("item_{}.word.{}.syntactic_category".format(index, query_item.str_type))
+            index_part += "_cat"
+        return index_part, keys
+
+
+class QueryExecutionContext(object):
+    def __init__(self, keys, collection):
+        self.keys = keys
+        self.collection = collection
+
+
+class QueryBuilder(object):
+    def __init__(self, collection_map):
+        self._collection_map = collection_map
+
+    def build_query(self, query_container, target_comes_after):
+        index_name, keys = query_container.create_context(target_comes_after)
+        collection = self._collection_map[query_container._n]
+
+        index_keys = [(key, pymongo.ASCENDING) for key in keys]
+        collection.ensure_index(index_keys, name=index_name, drop_dups=True)
+
+        return QueryExecutionContext(keys, collection)
+
+
+class QueryExecutor(object):
+    def __init__(self):
+        self._query_execution_context = None
+        self._params = None
+
+    def query_execution_context(self, query_execution_context):
+        self._query_execution_context = query_execution_context
+        return self
+
+    def params(self, *args):
+        self._params = list(args)
+        return  self
+
+    def find(self):
+        query_with_params = self._build_query_with_params()
+        return self._query_execution_context.collection.find(query_with_params).count()
+
+    def count(self):
+        return self.find().count()
+
+    def _build_query_with_params(self):
+        assert len(self._params)==len(self._query_execution_context.keys)
+        mongo_query = {}
+        for index, param in enumerate(self._params):
+            mongo_query[self._query_execution_context.keys[index]] = param
+
+        return mongo_query
---
+++ trnltk/statistics/test/test_query.py
@@ -0,0 +1,184 @@
+# coding=utf-8
+import unittest
+from hamcrest import *
+from mock import Mock
+from trnltk.statistics.query import WordNGramQueryContainer, QueryBuilder
+
+class WordNGramQueryContainerTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_add_criterion(self):
+        self.assertRaises(AssertionError, lambda: WordNGramQueryContainer(-1))
+        self.assertRaises(AssertionError, lambda: WordNGramQueryContainer(0))
+
+        assert_that(str(WordNGramQueryContainer(1).target_surface()._target_item), equal_to("(surface)"))
+        assert_that(str(WordNGramQueryContainer(1).target_surface(False)._target_item), equal_to("(surface)"))
+        assert_that(str(WordNGramQueryContainer(1).target_surface(True)._target_item), equal_to("(surface, syntactic_category)"))
+
+        assert_that(str(WordNGramQueryContainer(2).target_surface()._target_item), equal_to("(surface)"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False)._target_item), equal_to("(surface)"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(True)._target_item), equal_to("(surface, syntactic_category)"))
+
+        assert_that(str(WordNGramQueryContainer(2).target_surface().given_stem()._target_item), equal_to("(surface)"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False).given_stem(False)._target_item), equal_to("(surface)"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False).given_stem(True)._target_item), equal_to("(surface)"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(True).given_stem(True)._target_item), equal_to("(surface, syntactic_category)"))
+
+        assert_that(str(WordNGramQueryContainer(2).target_surface()._given_items), equal_to("[]"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False)._given_items), equal_to("[]"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(True)._given_items), equal_to("[]"))
+
+        assert_that(str(WordNGramQueryContainer(2).target_surface().given_stem()._given_items), equal_to("[(stem)]"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False).given_stem(False)._given_items), equal_to("[(stem)]"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False).given_stem(True)._given_items), equal_to("[(stem, syntactic_category)]"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(True).given_stem(True)._given_items), equal_to("[(stem, syntactic_category)]"))
+
+        assert_that(str(WordNGramQueryContainer(2).target_surface()._given_items), equal_to("[]"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False)._given_items), equal_to("[]"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(True)._given_items), equal_to("[]"))
+
+        assert_that(str(WordNGramQueryContainer(2).target_surface().given_stem()._given_items), equal_to("[(stem)]"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False).given_stem(False)._given_items), equal_to("[(stem)]"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False).given_stem(True)._given_items), equal_to("[(stem, syntactic_category)]"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(True).given_stem(True)._given_items), equal_to("[(stem, syntactic_category)]"))
+
+
+
+        assert_that(str(WordNGramQueryContainer(3).target_surface()._given_items), equal_to("[]"))
+        assert_that(str(WordNGramQueryContainer(3).target_surface(False)._given_items), equal_to("[]"))
+        assert_that(str(WordNGramQueryContainer(3).target_surface(True)._given_items), equal_to("[]"))
+
+        assert_that(str(WordNGramQueryContainer(3).target_surface().given_stem().given_stem()._given_items), equal_to("[(stem), (stem)]"))
+        assert_that(str(WordNGramQueryContainer(3).target_surface(False).given_stem(True).given_stem()._given_items), equal_to("[(stem, syntactic_category), (stem)]"))
+        assert_that(str(WordNGramQueryContainer(3).target_surface(False).given_stem(True).given_stem(True)._given_items), equal_to("[(stem, syntactic_category), (stem, syntactic_category)]"))
+        assert_that(str(WordNGramQueryContainer(3).target_surface(True).given_stem(False).given_stem(True)._given_items), equal_to("[(stem), (stem, syntactic_category)]"))
+
+    def test_create_context_index_name_without_given(self):
+        assert_that(WordNGramQueryContainer(1).target_surface(False).create_context(False)[0], equal_to("word1GramIdx_0_surface"))
+        assert_that(WordNGramQueryContainer(1).target_surface(False).create_context(True )[0], equal_to("word1GramIdx_0_surface"))
+
+        assert_that(WordNGramQueryContainer(1).target_surface(True ).create_context(False)[0], equal_to("word1GramIdx_0_surface_cat"))
+        assert_that(WordNGramQueryContainer(1).target_surface(True ).create_context(True )[0], equal_to("word1GramIdx_0_surface_cat"))
+
+        assert_that(WordNGramQueryContainer(1).target_surface(False).create_context(False)[0], equal_to("word1GramIdx_0_surface"))
+        assert_that(WordNGramQueryContainer(1).target_surface(False).create_context(True )[0], equal_to("word1GramIdx_0_surface"))
+
+    def test_create_context_index_name_without_target(self):
+        assert_that(WordNGramQueryContainer(1).given_surface(False).create_context(False)[0], equal_to("word1GramIdx_0_surface"))
+        assert_that(WordNGramQueryContainer(1).given_surface(False).create_context(True )[0], equal_to("word1GramIdx_0_surface"))
+
+        assert_that(WordNGramQueryContainer(2).given_surface(True ).given_surface(True ).create_context(False)[0], equal_to("word2GramIdx_0_surface_cat_1_surface_cat"))
+        assert_that(WordNGramQueryContainer(2).given_surface(True ).given_surface(False).create_context(True )[0], equal_to("word2GramIdx_0_surface_cat_1_surface"))
+
+        assert_that(WordNGramQueryContainer(3).given_surface(True ).given_surface(False).given_surface(True ).create_context(False)[0], equal_to("word3GramIdx_0_surface_cat_1_surface_2_surface_cat"))
+        assert_that(WordNGramQueryContainer(3).given_surface(True ).given_surface(False).given_surface(False).create_context(True )[0], equal_to("word3GramIdx_0_surface_cat_1_surface_2_surface"))
+
+    def test_create_context_index_name_with_target_and_given(self):
+        assert_that(WordNGramQueryContainer(2).target_surface(False).given_surface(False).create_context(False)[0], equal_to("word2GramIdx_0_surface_1_surface"))
+        assert_that(WordNGramQueryContainer(2).target_surface(True ).given_surface(False).create_context(True )[0], equal_to("word2GramIdx_1_surface_cat_0_surface"))
+
+        assert_that(WordNGramQueryContainer(2).target_surface(False).given_surface(True ).create_context(False)[0], equal_to("word2GramIdx_0_surface_1_surface_cat"))
+        assert_that(WordNGramQueryContainer(2).target_surface(False).given_surface(True ).create_context(True )[0], equal_to("word2GramIdx_1_surface_0_surface_cat"))
+
+        assert_that(WordNGramQueryContainer(3).target_surface(True ).given_surface(False).given_stem(False).create_context(False)[0], equal_to("word3GramIdx_0_surface_cat_1_surface_2_stem"))
+        assert_that(WordNGramQueryContainer(3).target_surface(True ).given_surface(False).given_stem(True ).create_context(True )[0], equal_to("word3GramIdx_2_surface_cat_0_surface_1_stem_cat"))
+
+        assert_that(WordNGramQueryContainer(3).target_surface(False).given_surface(True ).given_stem(False).create_context(False)[0], equal_to("word3GramIdx_0_surface_1_surface_cat_2_stem"))
+        assert_that(WordNGramQueryContainer(3).target_surface(False).given_surface(True ).given_stem(False).create_context(True )[0], equal_to("word3GramIdx_2_surface_0_surface_cat_1_stem"))
+
+        assert_that(WordNGramQueryContainer(4).target_surface(False).given_surface(True ).given_stem(False).given_lemma_root(False).create_context(False)[0], equal_to("word4GramIdx_0_surface_1_surface_cat_2_stem_3_lemma_root"))
+        assert_that(WordNGramQueryContainer(4).target_surface(True ).given_surface(True ).given_stem(False).given_lemma_root(True).create_context(True )[0], equal_to("word4GramIdx_3_surface_cat_0_surface_cat_1_stem_2_lemma_root_cat"))
+
+    def test_create_context_keys_without_given(self):
+        assert_that(WordNGramQueryContainer(1).target_surface(False).create_context(False)[1], equal_to(['item_0.word.surface.value']))
+        assert_that(WordNGramQueryContainer(1).target_surface(False).create_context(True )[1], equal_to(['item_0.word.surface.value']))
+
+        assert_that(WordNGramQueryContainer(1).target_surface(True ).create_context(False)[1], equal_to(['item_0.word.surface.value', 'item_0.word.surface.syntactic_category']))
+        assert_that(WordNGramQueryContainer(1).target_surface(True ).create_context(True )[1], equal_to(['item_0.word.surface.value', 'item_0.word.surface.syntactic_category']))
+
+        assert_that(WordNGramQueryContainer(1).target_surface(False).create_context(False)[1], equal_to(['item_0.word.surface.value']))
+        assert_that(WordNGramQueryContainer(1).target_surface(False).create_context(True )[1], equal_to(['item_0.word.surface.value']))
+
+    def test_create_context_keys_without_target(self):
+        assert_that(WordNGramQueryContainer(1).given_surface(False).create_context(False)[1], equal_to(['item_0.word.surface.value']))
+        assert_that(WordNGramQueryContainer(1).given_surface(False).create_context(True )[1], equal_to(['item_0.word.surface.value']))
+
+        assert_that(WordNGramQueryContainer(2).given_surface(True ).given_surface(True ).create_context(False)[1], equal_to(['item_0.word.surface.value', 'item_0.word.surface.syntactic_category', 'item_1.word.surface.value', 'item_1.word.surface.syntactic_category']))
+        assert_that(WordNGramQueryContainer(2).given_surface(True ).given_surface(False).create_context(True )[1], equal_to(['item_0.word.surface.value', 'item_0.word.surface.syntactic_category', 'item_1.word.surface.value']))
+
+        assert_that(WordNGramQueryContainer(3).given_surface(True ).given_surface(False).given_surface(True ).create_context(False)[1], equal_to(['item_0.word.surface.value', 'item_0.word.surface.syntactic_category', 'item_1.word.surface.value', 'item_2.word.surface.value', 'item_2.word.surface.syntactic_category']))
+        assert_that(WordNGramQueryContainer(3).given_surface(True ).given_surface(False).given_surface(False).create_context(True )[1], equal_to(['item_0.word.surface.value', 'item_0.word.surface.syntactic_category', 'item_1.word.surface.value', 'item_2.word.surface.value']))
+
+    def test_create_context_index_name_with_target_and_given(self):
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False).given_surface(False).create_context(False)[1]), equal_to("['item_0.word.surface.value', 'item_1.word.surface.value']"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(True ).given_surface(False).create_context(True )[1]), equal_to("['item_1.word.surface.value', 'item_1.word.surface.syntactic_category', 'item_0.word.surface.value']"))
+
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False).given_surface(True ).create_context(False)[1]), equal_to("['item_0.word.surface.value', 'item_1.word.surface.value', 'item_1.word.surface.syntactic_category']"))
+        assert_that(str(WordNGramQueryContainer(2).target_surface(False).given_surface(True ).create_context(True )[1]), equal_to("['item_1.word.surface.value', 'item_0.word.surface.value', 'item_0.word.surface.syntactic_category']"))
+
+        assert_that(str(WordNGramQueryContainer(3).target_surface(True ).given_surface(False).given_stem(False).create_context(False)[1]), equal_to("['item_0.word.surface.value', 'item_0.word.surface.syntactic_category', 'item_1.word.surface.value', 'item_2.word.stem.value']"))
+        assert_that(str(WordNGramQueryContainer(3).target_surface(True ).given_surface(False).given_stem(True ).create_context(True )[1]), equal_to("['item_2.word.surface.value', 'item_2.word.surface.syntactic_category', 'item_0.word.surface.value', 'item_1.word.stem.value', 'item_1.word.stem.syntactic_category']"))
+
+        assert_that(str(WordNGramQueryContainer(3).target_surface(False).given_surface(True ).given_stem(False).create_context(False)[1]), equal_to("['item_0.word.surface.value', 'item_1.word.surface.value', 'item_1.word.surface.syntactic_category', 'item_2.word.stem.value']"))
+        assert_that(str(WordNGramQueryContainer(3).target_surface(False).given_surface(True ).given_stem(False).create_context(True )[1]), equal_to("['item_2.word.surface.value', 'item_0.word.surface.value', 'item_0.word.surface.syntactic_category', 'item_1.word.stem.value']"))
+
+        assert_that(str(WordNGramQueryContainer(4).target_surface(False).given_surface(True ).given_stem(False).given_lemma_root(False).create_context(False)[1]), equal_to("['item_0.word.surface.value', 'item_1.word.surface.value', 'item_1.word.surface.syntactic_category', 'item_2.word.stem.value', 'item_3.word.lemma_root.value']"))
+        assert_that(str(WordNGramQueryContainer(4).target_surface(True ).given_surface(True ).given_stem(False).given_lemma_root(True).create_context(True )[1]), equal_to("['item_3.word.surface.value', 'item_3.word.surface.syntactic_category', 'item_0.word.surface.value', 'item_0.word.surface.syntactic_category', 'item_1.word.stem.value', 'item_2.word.lemma_root.value', 'item_2.word.lemma_root.syntactic_category']"))
+
+class QueryBuilderTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.unigram_collection = Mock(name="unigram_collection")
+        cls.bigram_collection = Mock(name="bigram_collection")
+        cls.trigram_collection = Mock(name="trigram_collection")
+        cls.fourgram_collection = Mock("fourgram_collection")
+
+        cls.collection_map = {
+            1: cls.unigram_collection,
+            2: cls.bigram_collection,
+            3: cls.trigram_collection,
+            4: cls.fourgram_collection
+        }
+
+        cls.query_builder = QueryBuilder(cls.collection_map)
+
+    def test_build_query_for_unigram(self):
+        mock_query_container = Mock()
+        mock_query_container._n = 1
+        mock_query_container.create_context.return_value = ("idx_name", ["key1", "key2"])
+
+        query_execution_context = self.query_builder.build_query(mock_query_container, True)
+
+        assert_that(query_execution_context.keys, equal_to(["key1", "key2"]))
+        assert_that(query_execution_context.collection, is_(self.unigram_collection))
+
+        self.unigram_collection.ensure_index.assert_called_with([('key1', 1), ('key2', 1)], drop_dups=True, name='idx_name')
+
+    def test_build_query_for_bigram(self):
+        mock_query_container = Mock()
+        mock_query_container._n = 2
+        mock_query_container.create_context.return_value = ("idx_name", ["key1", "key2"])
+
+        query_execution_context = self.query_builder.build_query(mock_query_container, True)
+
+        assert_that(query_execution_context.keys, equal_to(["key1", "key2"]))
+        assert_that(query_execution_context.collection, is_(self.bigram_collection))
+
+        self.bigram_collection.ensure_index.assert_called_with([('key1', 1), ('key2', 1)], drop_dups=True, name='idx_name')
+
+    def test_build_query_for_trigram(self):
+        mock_query_container = Mock()
+        mock_query_container._n = 3
+        mock_query_container.create_context.return_value = ("idx_name", ["key1", "key2"])
+
+        query_execution_context = self.query_builder.build_query(mock_query_container, True)
+
+        assert_that(query_execution_context.keys, equal_to(["key1", "key2"]))
+        assert_that(query_execution_context.collection, is_(self.trigram_collection))
+
+        self.trigram_collection.ensure_index.assert_called_with([('key1', 1), ('key2', 1)], drop_dups=True, name='idx_name')
+
+if __name__ == '__main__':
+    unittest.main()
