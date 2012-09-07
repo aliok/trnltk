4e0fd4c41ba2e8642eeda5089787b57ce0a02c69
Ali Ok <aliok@apache.org>
2012-09-07 17:21:19 +0200

    Tokenizer

---
+++ trnltk/tokenizer/test/test_texttokenizer.py
@@ -0,0 +1,111 @@
+# coding=utf-8
+import unittest
+from hamcrest import *
+from trnltk.tokenizer.texttokenizer import TextTokenizer
+
+class TextTokenizerTest(unittest.TestCase):
+    def setUp(self):
+        self.tokenizer = TextTokenizer()
+
+    def test_should_tokenize_text(self):
+        text = u"""Fiyatları uçuşa geçti."""
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'Fiyatları', u'uçuşa', u'geçti', u'.'))
+
+        text = u"""Fiyatları uçuşa geçti. """
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'Fiyatları', u'uçuşa', u'geçti', u'.'))
+
+        text = u""" Fiyatları uçuşa geçti."""
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'Fiyatları', u'uçuşa', u'geçti', u'.'))
+
+        text = u"""Fiyatları uçuşa geçti ."""
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'Fiyatları', u'uçuşa', u'geçti', u'.'))
+
+        text = u"""\r\t\p\nFiyatları\n \t\r\n  uçuşa \rgeçti .   \t"""
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'Fiyatları', u'uçuşa', u'geçti', u'.'))
+
+
+    def test_should_tokenize_text_with_exceptions(self):
+        text = u"""\r\tABD'de\n elma fiyatları\n \t\r\n  uçuşa \rgeçti .   \t"""
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'ABD\'de', u'elma', u'fiyatları', u'uçuşa', u'geçti', u'.'))
+
+        text = u"""\r\tABD'de\n elma fiyatları\n \t\r\n  uçuşa \rgeçti ..   \t"""
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'ABD\'de', u'elma', u'fiyatları', u'uçuşa', u'geçti', u'..'))
+
+        text = u"""\r\tABD'de\n elma fiyatları\n \t\r\n  uçuşa \rgeçti !..   \t"""
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'ABD\'de', u'elma', u'fiyatları', u'uçuşa', u'geçti', u'!..'))
+
+        text = u"""\r\tABD'de\n elma fiyatları\n \t\r\n 3.  uçuşa \rgeçti .   \t"""
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'ABD\'de', u'elma', u'fiyatları', u'3.', u'uçuşa', u'geçti', u'.'))
+
+        text = u"""\r\tABD'de\n elma fiyatları\n \t\r\n 5'te uçuşa \rgeçti .   \t"""
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'ABD\'de', u'elma', u'fiyatları', u'5\'te', u'uçuşa', u'geçti', u'.'))
+
+        text = u"""\r\tABD'de\n elma fiyatları\n \t\r\n 5.'de uçuşa \rgeçti .   \t"""
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'ABD\'de', u'elma', u'fiyatları', u'5.\'de', u'uçuşa', u'geçti', u'.'))
+
+        text = u"5:20 gibi gelecek."
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'5:20', u'gibi', u'gelecek', u'.'))
+
+        text = u"5,60 TL verdim."
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'5,60', u'TL', u'verdim', u'.'))
+
+        text = u"5.600 TL verdim."
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'5.600', u'TL', u'verdim', u'.'))
+
+        text = u"5.600,1234 TL verdim."
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'5.600,1234', u'TL', u'verdim', u'.'))
+
+        text = u"100'le gidiyor."
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'100\'le', u'gidiyor', u'.'))
+
+        text = u"5:20'de geliyorum."
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'5:20\'de', u'geliyorum', u'.'))
+
+        text = u"5,74'te bir ihtimal."
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'5,74\'te', u'bir', u'ihtimal', u'.'))
+
+        text = u"6, 7 ve 8 numara gelsin."
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'6', u'7', u've', u'8', u'numara', u'gelsin', u'.'))
+
+        text = u"6., 7. ve 8. adamlar gelsin."
+        tokens = self.tokenizer.tokenize(text)
+        assert_that(tokens, has_items(u'6.', u'7.', u've', u'8.', u'adamlar', u'gelsin', u'.'))
+
+
+    def test_should_tokenize_longer_text(self):
+        text = u"""
+ABD'den gelen veriyle altın fiyatları uçuşa geçti.
+ABD'de tarım dışı istihdamın beklentilerin altında artmasının ardından küresel piyasalarda ABD Merkez Bankası'nın yavaşlayan ekonomiye önlem olarak yeni bir parasal gevşemeye gideceğine yönelik beklentilerle altın 6 ayın en yüksek düzeyini gördü.
+Altının ons fiyatı ABD'den 15.30'da gelen verinin hemen ardından dakikalar içinde yüzde 1.5 yükselişle 1730 dolara kadar yükseldi.
+ABD'den gelen zayıf veriler yatırımcıları güvenli liman olan altına yönlendiriyor.
+Altının yıl başından bu yana değer kazancı yüzde 10'u aştı.
+Uluslararası piyasalarda altının onsu yıl içerinde en düşük 1527,22 doları en yüksek ise 1790,79 doları gördü.
+        """
+        tokens = self.tokenizer.tokenize(text)
+
+        assert_that('' not in tokens)                                   # no empty text
+        assert_that(all([lambda x : all([c not in x for c in [u'\r', u'\n', u' ']]) for x in tokens]))     # no whitespace
+        for a in tokens:
+            print u'=={}=='.format(a)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ trnltk/tokenizer/texttokenizer.py
@@ -0,0 +1,81 @@
+class TextTokenizer(object):
+    """
+    Splits the text into words.
+    """
+
+    PUNC_CHARS = [u'.', u',', u'..', u'...', u'-', u'!', u'?', u':', u'"', u'(', u')']     #  " ' " is not included
+
+    def tokenize(self, text):
+        """
+        @type text: str or unicode
+        @rtype list or None
+        """
+
+        text = text.strip()
+
+        text = self._insert_spaces_for_punc_chars(text)
+
+        text = self._replace_all_space_with_whitespace(text)
+
+        tokens = text.split(u' ')
+
+        tokens = filter(lambda x : x, tokens)
+
+        return tokens
+
+    def _insert_spaces_for_punc_chars(self, text):
+        # insert space before punc chars. except:
+
+        # 1. Dot comes after number : "3."
+        # 2. Punc char comes after punc char : "..." (except comma after dot)
+        # 3. The apostrophe used in a proper name : "Ahmet'in"  # apostrophe is not a punc char anymore
+        # 4. The quoted stuff for emphasis : 'Cok "aptal"di o adam' #TODO
+        # 5. Punc chars between numbers : "5:20", "5.123.456", "5,12" (but not "5, 6, 7")
+
+        # use a string builder(?). add chars directly, except we need to insert space in front of them.
+        # with a random access list(string builder), we can check the last char for the exception cases described above
+
+        builder = u''
+        for char_index, char in enumerate(text):
+            if not char in self.PUNC_CHARS:
+                builder = builder + char
+            else:
+                if not builder:
+                    continue
+
+                prev_char = builder[-1]
+                next_char = None if char_index+1>=len(text) else text[char_index+1]
+
+                add_space = True
+
+                # check case #1
+                if prev_char and prev_char.isdigit() and char == u'.':
+                    add_space = False
+
+                # check case #2
+                elif prev_char in self.PUNC_CHARS:
+                    if char==u',' and prev_char==u'.':
+                        add_space = True
+                    else:
+                        add_space = False
+
+                # check case #5
+                elif prev_char.isdigit() and next_char and next_char.isdigit():
+                    add_space = False
+
+                if add_space:
+                    builder += u' '
+                builder = builder + char
+
+        return builder
+
+    def _replace_all_space_with_whitespace(self, text):
+
+        """
+        @type text: str or unicode
+        """
+        text = text.replace(u'\n', u' ')
+        text = text.replace(u'\t', u' ')
+        text = text.replace(u'\r', u' ')
+
+        return text
